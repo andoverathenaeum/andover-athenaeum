e4da3d76b9f4961fe84861578e0eb8f5a85a278e
Nakul Iyer <nakulpiyer@gmail.com>
2020-12-19 04:14:09 -0500

    kaguya sama

---
+++ src/layouts/footer.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react'
+import Link from 'next/link'
+import Image from 'next'
+
+export function Footer() {
+  return (
+    <nav className="bg-gray-800 mt-12 z-50 w-screen">
+      <div className="inside-bottom-banner">
+        <ul className="bottom-banner-li roman text-md text-white pl-6 pt-2 flex text-center h-10">
+          <li><a href="https://andoverathenaeum.com/about">About</a></li>
+          <li><a href="mailto:andoverathenaeum@gmail.com">Contact us</a></li>
+          <li><a href="https://andoverathenaeum.com/privacy-policy">Privacy
+                                                                    Policy</a>
+          </li>
+          <li><a href="https://www.instagram.com/andoverathenaeum/"><img
+            src="/assets/instagram.png"
+            className="inline mr-2"
+            style={{ width: 20 }}
+            alt=""
+          />andoverathenaeum</a></li>
+          <li><a href="https://twitter.com/andoverathenae1"><img
+            src="/assets/twitter.png"
+            className="inline mr-2"
+            style={{ width: 20 }}
+            alt=""
+          />andoverathenae1</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
--- src/layouts/navigation.js
+++ src/layouts/navigation.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 import { Header } from './header'
+import { Footer } from './footer'
 
 export function Navigation({ children }) {
   return (
     <Fragment>
       <Header />
       <div className="bg-gray-50 border-t border-color-gray-200">{children}</div>
+      <Footer />
     </Fragment>
   )
 }
