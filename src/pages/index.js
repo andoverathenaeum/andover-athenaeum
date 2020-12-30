2316435cbede729918409d7c216bb7e504c9d817
Nakul Iyer <nakulpiyer@gmail.com>
2020-12-30 17:23:00 -0500

    link shit sorted out

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import React from 'react'
 import Markdown from '../components/markdown'
 
+import Link from 'next/link'
+
 export default function Home() {
     return (
         <div>
@@ -40,15 +42,17 @@ export default function Home() {
                     >
                         Phillips Academy's <span className="text-indigo-800">Classics Magazine</span>
                     </div>
-                    <button
-                        type="button"
-                        className="transition-all cinzel-md text-center tracking-wider leading-8 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none"
-                        style={{
-                            marginTop: 20
-                        }}
-                    >
-                        Read Our Latest Issue!
-                    </button>
+                    <Link href={'/latest'}>
+                        <button
+                            type="button"
+                            className="transition-all cinzel-md text-center tracking-wider leading-8 inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none"
+                            style={{
+                                marginTop: 20
+                            }}
+                        >
+                            Read Our Latest Issue!
+                        </button>
+                    </Link>
                 </div>
             </div>
             <div className="mt-6 sm:mt-12">
